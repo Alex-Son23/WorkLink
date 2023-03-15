54d404418e5a06a6c601b2899b73483ca0e6d2d5
RED <support@pleshakov.org>
2023-03-15 13:45:19 +0500

    ADD. Add index page view. Add project markup files.

--- worklink/worklink/urls.py
+++ worklink/worklink/urls.py
@@ -1,6 +1,8 @@
-from django.conf.urls import url
+from django.urls import re_path
 from django.contrib import admin
+from worklink.views import index
 
 urlpatterns = [
-    url(r"^admin/", admin.site.urls),
+    re_path(r"^admin/", admin.site.urls),
+    re_path(r"^$", index, name='main'),
 ]
