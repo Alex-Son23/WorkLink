e5e1e08002a6f7b23b7d7a3fa4b446668222e145
alex <sashason1990@gmail.com>
2023-03-16 22:04:39 +0500

    added authapp application

---
+++ worklink/authapp/forms.py
@@ -0,0 +1,82 @@
+import hashlib
+import random
+
+from django.contrib.auth.forms import AuthenticationForm, UserCreationForm, UserChangeForm
+from django import forms
+from django.contrib.auth.models import User
+
+from authapp.models import  ShopUserProfile
+
+
+class ShopUserLoginForm(AuthenticationForm):
+    class Meta:
+        model = User
+        fields = ('username', 'password')
+
+    def __init__(self, *args, **kwargs):
+        super(ShopUserLoginForm, self).__init__(*args, **kwargs)
+        for field_name, field in self.fields.items():
+            field.widget.attrs['class'] = 'form-control'
+
+
+class ShopUserProfileForm(forms.ModelForm):
+    class Meta:
+        model = ShopUserProfile
+        fields = ('tagline', 'about_me', 'gender')
+
+    def __init__(self, *args, **kwargs):
+        super(ShopUserProfileForm, self).__init__(*args, **kwargs)
+        for field_name, field in self.fields.items():
+            field.widget.attrs['class'] = 'form-control'
+
+
+class ShopUserRegisterForm(UserCreationForm):
+    class Meta:
+        model = User
+
+        fields = ('username', 'first_name', 'password1', 'password2', 'email',)
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+        for field_name, field in self.fields.items():
+            field.widget.attrs['class'] = 'form-control'
+            field.help_text = ''
+
+    def clean_age(self):
+        data = self.cleaned_data['age']
+
+        if data < 18:
+            raise forms.ValidationError("Вы слишком молоды!")
+        return data
+
+    def save(self):
+        user = super(ShopUserRegisterForm, self).save()
+
+        user.is_active = False
+        salt = hashlib.sha1(str(random.Random()).encode('utf8')).hexdigest()[:6]
+        user.activation_key = hashlib.sha1((user.email + salt).encode('utf8')).hexdigest()
+        user.save()
+
+        return user
+
+
+class ShopUserEditForm(UserChangeForm):
+    class Meta:
+        model = User
+        fields = ('username', 'first_name', 'email', 'password')
+
+    def __init__(self, *args, **kwargs):
+        super(ShopUserEditForm, self).__init__(*args, **kwargs)
+        for field_name, field in self.fields.items():
+            field.widget.attrs['class'] = 'form-control'
+            field.help_text = ''
+            if field_name == 'password':
+                field.widget = forms.HiddenInput()
+
+    def clean_age(self):
+        data = self.cleaned_data['age']
+        # print(self.data)
+        if data < 18:
+            raise forms.ValidationError("Вы слишком молоды!")
+        return data
--- worklink/worklink/urls.py
+++ worklink/worklink/urls.py
@@ -1,10 +1,10 @@
 from worklink import views
-from django.urls import path
+from django.urls import path, include
 from django.contrib import admin
 
 urlpatterns = [
     path('admin/', admin.site.urls),
 
-    path('', views.IndexView.as_view()),
-
+    path('', views.IndexView.as_view(), name='index'),
+    path('auth/', include('authapp.urls', namespace='auth')),
 ]
