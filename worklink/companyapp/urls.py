f1af759bcc696eea80fbfacbfd0121a546c487e7
alex <sashason1990@gmail.com>
2023-03-29 01:14:04 +0500

    edited urls

--- worklink/companyapp/urls.py
+++ worklink/companyapp/urls.py
@@ -5,8 +5,8 @@ app_name = "companyapp"
 from django.urls import path
 
 urlpatterns = [
-    path('vacancies/', VacancyListView.as_view(), name='vacancies'),
-    path('vacancies/add/', VacancyCreateView.as_view(), name='vacancy_add'),
-    path('vacancies/<int:pk>/', VacancyDetailView.as_view(), name='vacancy_detail'),
-    path('vacancies/<int:pk>/edit/', VacancyUpdateView.as_view(), name='vacancy_edit'),
+    path('my-vacancies/', VacancyListView.as_view(), name='vacancies'),
+    path('my-vacancies/add/', VacancyCreateView.as_view(), name='vacancy_add'),
+    path('my-vacancies/<int:pk>/', VacancyDetailView.as_view(), name='vacancy_detail'),
+    path('my-vacancies/<int:pk>/edit/', VacancyUpdateView.as_view(), name='vacancy_edit'),
 ]
